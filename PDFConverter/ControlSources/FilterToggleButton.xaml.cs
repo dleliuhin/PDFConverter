fbb323dc9b00bdfdd76575cd1419e3b994d7fb42
Dmitrii Leliuhin <dleliuhin@mail.ru>
2019-02-01 02:57:04 +0300

    Add Filter Toggle Button
    Add Filter Toggle Button to the preview UI

---
+++ PDFConverter/ControlSources/FilterToggleButton.xaml.cs
@@ -0,0 +1,40 @@
+﻿/*!
+ * @file FilterToggleButton.xaml.cs
+ * 
+ * @author Dmitrii Leliuhin dleliuhin@mail.ru
+ * 
+ * @date 01.02.2019 2:51:44
+ * 
+ * @todo 
+ * 
+ * @bug No known bugs.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDFConverter
+{
+    /*! \class FilterToggleButton
+     *  \brief Interaction logic for FilterToggleButton.xaml.
+     */
+    public partial class FilterToggleButton : UserControl
+    {
+        public FilterToggleButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
